7183c32b6405876c0c13fb624b0e767ebfac7d03
Bernard Chisumo <bchisumo74@yahoo.com>
2017-03-07 17:17:06 +0000

    Logging in Multiple users

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/admin', 'AdminController@index');
+
+/* Route Group*/
+
+Route::prefix('admin')->group(function() {
+
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+
+});
+
+//Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+//Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+//Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
